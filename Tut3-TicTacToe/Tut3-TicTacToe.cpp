5e05c0f10801ea5ac1a0eec5f67904544b2c50a7
Pavashlin <pavashlingovender@yahoo.com>
2015-03-15 15:27:54 +0200

    functionality

--- Tut3-TicTacToe/Tut3-TicTacToe.cpp
+++ Tut3-TicTacToe/Tut3-TicTacToe.cpp
@@ -1,6 +1,7 @@
 // Tut3-TicTacToe.cpp : Defines the entry point for the console application.
 //
 
+#include "TicTacToe.h"
 #include "stdafx.h"
 #include<iostream>
 
@@ -8,14 +9,18 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int x = 1,command;
+	int x = 1;
+	int command = 0;
+	int row, column;
 
+	TicTacToe game;
 	cout << "Game has started" << endl;
 	cout << "Player 1 begins" << endl;
 	
 	
 	while (x > 0)
 	{
+		cout << '\n' << endl;
 		cout << "enter command: " << endl;
 		cout << "1. Reset" << endl;
 		cout << "2. Move" << endl;
@@ -24,6 +29,32 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		cin >> command;
 
+		if (command = 1)
+		{
+			game.restart();
+			cout << '\n' << endl;
+		}
+
+		else if (command = 2)
+		{
+			cout << "Enter row" << endl;
+			cin >> row;
+			cout << "Enter column" << endl;
+			cin >> column;
+
+			game.move(row, column);
+		}
+		else if (command = 3)
+		{
+			game.print();
+			cout << '\n' << endl;
+		}
+
+		else if(command = 4)
+		{
+			game.isWon();
+			cout << '\n' << endl;
+		}
 
 	}
 	
